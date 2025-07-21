988471b4a576f369cff105fde310da5cf4c9fd39
jackLDyer <104628873+jackLDyer@users.noreply.github.com>
2025-07-21 13:34:59 +0100

    demo version ready

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,28 +97,11 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              href: 'https://discord.gg/2tdA2zGzkJ',
             },
           ],
         },
@@ -126,17 +109,13 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
+              label: 'Docusaurus',
               href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Built Diff Guides. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
